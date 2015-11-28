d59ca34d8b9b1a8d7b36791033ba22bdc7129d9c
matt.j.booth@googlemail.com <matt.j.booth@googlemail.com>
2015-11-28 11:27:39 +0000

    Remove Gulp and add Grunt + node modules

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+module.exports = function(grunt) {
+
+grunt.initConfig({
+  uglify: {
+    options: {
+      mangle: false
+    },
+    my_target: {
+      files: {
+        '__build/assets/com-scripts.min.js': ['assets/com-scripts.js']
+      }
+    }
+  }
+});
+
+grunt.registerTask('default', ['uglify']);
+grunt.loadNpmTasks('grunt-contrib-uglify');
+
+};
