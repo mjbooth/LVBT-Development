ec41cd92c411fcfede614dcdafcad8360bf64e91
matt.j.booth@googlemail.com <matt.j.booth@googlemail.com>
2015-12-19 18:01:03 +0000

    New Gruntfile to add cssmin task

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,10 +10,28 @@ grunt.initConfig({
         'assets/com-scripts.min.js': ['assets/com-scripts.js']
       }
     }
+  },
+
+  cssmin: {
+    options: {
+    shorthandCompacting: true,
+    roundingPrecision: -1,
+  },
+    css: {
+      files: [{
+        expand: true,
+        cwd: 'assets',
+        src: ['*.css', '!*.min.css'],
+        dest: 'assets',
+        ext: '.min.css'
+      }]
+    }
   }
+
 });
 
-grunt.registerTask('default', ['uglify']);
+grunt.registerTask('default', ['uglify'], ['cssmin'] );
 grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-contrib-cssmin');
 
 };
